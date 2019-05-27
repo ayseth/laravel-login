752c4a3d61557592058d0110408683637d5bf6ce
A.Y Seth <aysait101@gmail.com>
2019-05-27 02:28:46 +0100

    setting mailgun

--- config/mail.php
+++ config/mail.php
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'from' => ['address' => null, 'name' => null],
+    'from' => ['address' => 'aysait101@gmail.com', 'name' => 'Abir Seth'],
 
     /*
     |--------------------------------------------------------------------------
